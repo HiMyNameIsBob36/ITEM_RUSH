69d3335c612698ec930e50e56f82028afb317f1c
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-10 10:40:39 +1100

    Update check.js

--- BP/functions/rush/check.js
+++ BP/functions/rush/check.js
@@ -1,4 +1,7 @@
 ## Check >> Item
-execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 6 
-execute if score item rushDB 2 if entity @a[tag=runner,hasitem={item=shears}] run scoreboard players set stage rushDB 5
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 2 if entity @a[tag=runner,hasitem={item=shears}] run scoreboard players set stage rushDB 3
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3 
+execute if score item rushDB 1 if entity @a[tag=runner,hasitem={item=flint_and_steel}] run scoreboard players set stage rushDB 3
 ## Check >> Death
